25cef738858c1d464c4fa73fa50171317bd50f45
P81000 <p.masteguin@unifesp.br>
2023-01-12 13:57:15 -0300

    initial variables

---
+++ EX05/programa5.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define NMAX 1000
+#define CMAX 1000000
+
+int main() 
+{
+    int N, C, T1, T2;
+    scanf("%d %d %d %d", &N, &C, &T1, &T2);
+    return 0;
+}
